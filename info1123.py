5d01a7820a2e2fb7817ce73ced636af1b887dc03
der-kaffe <jcardenas2022@alu.uct.cl>
2022-10-08 12:08:02 -0300

    casi

--- info1123.py
+++ info1123.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import csv
 
 # Creamos clase padre
 
@@ -9,11 +10,38 @@ class Contenedores:
     masa: str
     kilos: int
 
+    def kilos(self):
+        return self.kilos
+
 
 @dataclass
 class Vehiculos:
     Costo = int
     capacidad = int
+
+
+@dataclass
+class Barco(Vehiculos):
+    Costo = 1000000000
+    Capacidad = 24000
+
+
+@dataclass
+class Tren(Vehiculos):
+    Costo = 10000000
+    Capacidad = 250
+
+
+@dataclass
+class Avion(Vehiculos):
+    Costo = 1000000
+    Capacidad = 10
+
+
+@dataclass
+class Camion(Vehiculos):
+    Costo = 500000
+    Capacidad = 1
 
 
 @dataclass
@@ -45,3 +73,49 @@ class Refrigerado_P(Contenedores):
 @dataclass
 class Refrigerado_G(Contenedores):
     tamaño: str = "grande"
+
+
+lista = []
+# Listas que contienen containers
+normal = []
+
+
+def leer():
+    with open("ejemplo_lista.csv") as f:
+        reader = csv.reader(f, delimiter=";")
+        # nos saltamos la primera linea
+        next(reader, None)
+        for row in reader:
+            lista.append(row)
+
+
+def container():
+    h = 0
+    for i in range(len(lista)):
+        if lista[i][2] == "normal" and lista[i][3] == "solida":
+            print(lista[i][4])
+            h += int(lista[i][4])
+    h /= 1000
+    # Sobra un 0.365 que en el .ods se desprecia, se hará igual
+    h = int(h)
+    # Lo dividimos en el T. del container grande
+    h /= 24000
+    # Lo restante (0.algo), que siempre será menor a 1, entonces es fh<24000 que es un Con.G
+    fh = h-int(h)
+    Peso_pequenho = (fh*24000)/100
+    # print(h) Sobra un 0.003 que son 830 gramos, ni siquiera un kilo si que lo despreciamos
+    h = int(h)
+    # Eso significa que tenemos 28 containers grandes normales
+    for i in range(h):
+        x = Contenedor_G("Normal", "Solida", 24000)
+        normal.append(x)
+    for i in range(1):
+        x = Contenedor_P("Normal", "Liquida", Peso_pequenho)
+        normal.append(x)
+    # for i in range(len(lista)):
+    #     if lista[i][2] == "normal" and lista[i][3] == "liquida":
+    #         print(lista[i][4])
+
+
+leer()
+container()
