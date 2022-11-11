3c8b4f76db4b0a3bddac2f080af4772883fa93a5
der-kaffe <jcardenas2022@alu.uct.cl>
2022-11-11 03:26:07 -0300

    :)

--- info1123.py
+++ info1123.py
@@ -1,11 +1,14 @@
 from dataclasses import dataclass
 import csv
+import sys
 from tkinter import *
 from tkinter import messagebox
 import tkinter as tk
 from PIL import ImageTk
 import mysql.connector
 from mysql.connector import errorcode
+from fpdf import FPDF
+
 
 try:
     cnx = mysql.connector.connect(user='A2022_jcardenas', password='A2022_jcardenas',
@@ -20,6 +23,14 @@ except mysql.connector.Error as err:
         print(err)
 
 cursor = cnx.cursor()
+
+# Creamos lo basico del pdf
+pdf = FPDF(orientation='P', unit='mm', format='A4')
+
+pdf.add_page()
+
+pdf.set_font('Arial', '', 10)
+
 
 # Creamos clase padre
 
@@ -55,6 +66,9 @@ class Vehiculos:
     def __init__(self, Costoo, Contenidoo):
         self.Costoo = Costo
         self.Contenidoo = Contenido
+
+    def contenido(self):
+        return self.Contenido()
 
 
 # ==========================================
@@ -360,6 +374,9 @@ def which_button4(button_press, transporte):
     texto = tk.Label(
         frame, text=f"{transporte} numero {[button_press]}\nCantidad de contenedores: {contenedor}\nCantidad de Estanques {estanque}\n\n\tP E S O S   T I P O DE C A R G A\n\nPeso Total: {peso}\nPeso Normal: {Peso_Normal}\nPeso Refrigerado: {Peso_Refrigerada}\nPeso Inflamable: {Peso_Inflamable}\n\n\tP E S O   M A S A\n\nPeso Solido: {solida}\nPeso liquido: {liquida}\n Peso gas: {gas}", wraplength=1100, background='white')
     texto.grid(column=0, row=0)
+    texto = tk.Label(
+        frame, text=f"{vehiculo[button_press].Contenido}", wraplength=1100, background='white')
+    texto.grid(column=1, row=1)
     ventana.update()
     c.config(scrollregion=c.bbox("all"))
 
@@ -384,7 +401,6 @@ def barcosV():
         if r == 26:
             h += 1
             r = 0
-
     if len(barcos) == 0:
         n = Label(frame, text="No hay Barcos")
         n.grid(column=1, row=2)
@@ -479,21 +495,35 @@ def camionesV():
 def recalcularBarcos(a, var):
     # Calculamos en valor multiplicando el precio por la cantidad de objetos en la lista
     numero = a * int(len(barcos))
+
+    pdf.multi_cell(
+        w=50, h=5, txt=f"\t\tprecios nuevos barco: {numero} ", align='C', fill=0, border=1)
+
     return var.set(numero)
 
 
 def recalcularTrenes(a, var2):
     # Calculamos en valor multiplicando el precio por la cantidad de objetos en la lista
-
     numero = a * int(len(trenes))
+    pdf.multi_cell(
+        w=50, h=5, txt=f"\t\tprecios nuevos trenes: {numero} ", align='C', fill=0, border=1)
     return var2.set(numero)
 
 
 def recalcularAviones(a, var3):
     # Calculamos en valor multiplicando el precio por la cantidad de objetos en la lista
-
     numero = a * int(len(aviones))
+    pdf.multi_cell(
+        w=50, h=5, txt=f"\t\tprecios nuevos aviones: {numero} ", align='C', fill=0, border=1)
     return var3.set(numero)
+
+
+def recalcularCamiones(a, var4):
+    # Calculamos en valor multiplicando el precio por la cantidad de objetos en la lista
+    numero = a * int(len(camiones))
+    pdf.multi_cell(
+        w=50, h=5, txt=f"\t\tprecios nuevos camiones: {numero} ", align='C', fill=0, border=1)
+    return var4.set(numero)
 
 
 def principal_venetana(b_precio, t_precio, a_precio, c_precio):
@@ -512,11 +542,16 @@ def principal_venetana(b_precio, t_precio, a_precio, c_precio):
     var4 = tk.StringVar()
     # Escribimos las caracteristicas generales pedidas
     totalvehiculos = Label(vp, text=f"Cantidad total de vehiculos: {total}")
+    pdf.multi_cell(
+        w=50, h=5, txt=f"Cantidad total de vehiculos: {str(total)}", align='C', fill=0, border=1)
     totalvehiculos.grid(column=1, row=1)
     # Imagenes y texto respectivo
     img = tk.PhotoImage(file="barco.png")
     totalvehiculos3 = Label(
         vp, text=f"\t\tCantidad: {len(barcos) }")
+    pdf.multi_cell(
+        w=50, h=5, txt=f"\t\tCantidad  barcos: {str(len(barcos)) }", align='C', fill=0, border=1)
+
     totalvehiculos3.grid(column=1, row=2)
 
     totalvehiculos3 = Label(
@@ -525,6 +560,8 @@ def principal_venetana(b_precio, t_precio, a_precio, c_precio):
     img2 = tk.PhotoImage(file="tren.png")
     totalvehiculos3 = Label(
         vp, text=f"\t\tCantidad: {len(trenes) }")
+    pdf.multi_cell(
+        w=50, h=5, txt=f"\t\tCantidad trenes: {str(len(trenes)) }", align='C', fill=0, border=1)
     totalvehiculos3.grid(column=1, row=3)
 
     totalvehiculos3 = Label(
@@ -533,6 +570,8 @@ def principal_venetana(b_precio, t_precio, a_precio, c_precio):
     img3 = tk.PhotoImage(file="avion.png")
     totalvehiculos3 = Label(
         vp, text=f"\t\tCantidad: {len(aviones) }")
+    pdf.multi_cell(
+        w=50, h=5, txt=f"\t\tCantidad aviones: {str(len(aviones)) }", align='C', fill=0, border=1)
     totalvehiculos3.grid(column=1, row=4)
 
     totalvehiculos3 = Label(
@@ -541,6 +580,8 @@ def principal_venetana(b_precio, t_precio, a_precio, c_precio):
     img4 = tk.PhotoImage(file="camion.png")
     totalvehiculos3 = Label(
         vp, text=f"\t\tCantidad: {len(camiones) }")
+    pdf.multi_cell(
+        w=50, h=5, txt=f"\t\tCantidad camiones: {str(len(camiones)) }", align='C', fill=0, border=1)
     totalvehiculos3.grid(column=1, row=5)
 
     totalvehiculos3 = Label(
@@ -548,10 +589,14 @@ def principal_venetana(b_precio, t_precio, a_precio, c_precio):
 
     totalvehiculos1 = Label(
         vp, text=f"Precio total de vehiculos: {len(barcos)*b_precio + len(trenes)*t_precio+len(aviones)*a_precio+len(camiones)*c_precio}")
+    pdf.multi_cell(
+        w=50, h=5, txt=f"Precio total de vehiculos: {str(len(barcos)*b_precio + len(trenes)*t_precio+len(aviones)*a_precio+len(camiones)*c_precio)}", align='C', fill=0, border=1)
     totalvehiculos1.grid(column=1, row=7)
 
     totalvehiculos1 = Label(
         vp, text=f"Precio de los barcos{len(barcos)*b_precio}")
+    pdf.multi_cell(
+        w=50, h=5, txt=f"Precio de los barcos {str(len(barcos)*b_precio)}", align='C', fill=0, border=1)
     totalvehiculos1.grid(column=1, row=8)
 
     totalvehiculos2 = Label(
@@ -577,12 +622,11 @@ def principal_venetana(b_precio, t_precio, a_precio, c_precio):
 
     btn4 = Button(vp, text="Camiones", command=camionesV)
     btn4.grid(column=1, row=15)
-    #El recalculo de los valores 
+    # El recalculo de los valores
     el = tk.Label(vp, text='Barcos')
     el.grid(column=1, row=16)
     entrada = tk.Entry(vp)
     entrada.grid(column=1, row=17)
-
     btn = tk.Button(vp, text="recalcular precio de barcos", fg='blue',
                     command=lambda: recalcularBarcos(int(entrada.get()), var))
     btn.grid(column=1, row=18)
@@ -590,7 +634,7 @@ def principal_venetana(b_precio, t_precio, a_precio, c_precio):
     res = tk.Label(vp, bg='plum', textvariable=var)
     res.grid(column=1, row=20)
 
-    ##El recalculo de los valores 
+    # El recalculo de los valores
     el = tk.Label(vp, text='Trenes')
     el.grid(column=1, row=21)
     entrada2 = tk.Entry(vp)
@@ -602,7 +646,7 @@ def principal_venetana(b_precio, t_precio, a_precio, c_precio):
     btn2.grid(column=1, row=23)
     res = tk.Label(vp, bg='plum', textvariable=var2)
     res.grid(column=1, row=24)
-    #El recalculo de los valores 
+    # El recalculo de los valores
     el = tk.Label(vp, text='Aviones')
     el.grid(column=1, row=25)
     entrada3 = tk.Entry(vp)
@@ -614,7 +658,7 @@ def principal_venetana(b_precio, t_precio, a_precio, c_precio):
     btn3.grid(column=1, row=27)
     res = tk.Label(vp, bg='plum', textvariable=var3)
     res.grid(column=1, row=28)
-    #El recalculo de los valores 
+    # El recalculo de los valores
     el = tk.Label(vp, text='Camiones')
     el.grid(column=1, row=29)
     entrada4 = tk.Entry(vp)
@@ -626,12 +670,81 @@ def principal_venetana(b_precio, t_precio, a_precio, c_precio):
     btn4.grid(column=1, row=31)
     res = tk.Label(vp, bg='plum', textvariable=var4)
     res.grid(column=1, row=34)
+
+    el = tk.Label(
+        vp, text='*La informacion se envia automaticamente a el pdf despues de cerrar el GUI', font=('Arial', 8))
+    el.grid(column=1, row=35)
+
     # Inicia el loop
+
     ventana.mainloop()
 
 
-leer()
-container()
-gas_liquido()
-transporte(b_precio, t_precio, a_precio, c_precio)
-principal_venetana(b_precio, t_precio, a_precio, c_precio)
+def escribirPDF():
+    contenedor = 0
+    estanque = 0
+    peso = 0
+    Peso_Normal = 0
+    Peso_Refrigerada = 0
+    Peso_Inflamable = 0
+    solida = 0
+    liquida = 0
+    gas = 0
+    for e in range(4):
+        if e == 0:
+            vehiculo = aviones
+        elif e == 1:
+            vehiculo = trenes
+        elif e == 2:
+            vehiculo = aviones
+        elif e == 3:
+            vehiculo = camiones
+        # Recorremos la lista .Contenido de los objetos y despues lo agregamos al contador
+        for i in range(len(vehiculo)):
+            for y in range(len(vehiculo[i].Contenido)):
+                if vehiculo[i].Contenido[y].masaa == "solida":
+                    solida += vehiculo[i].Contenido[y].kiloss
+                if vehiculo[i].Contenido[y].masaa == "liquida":
+                    liquida += vehiculo[i].Contenido[y].kiloss
+                if vehiculo[i].Contenido[y].masaa == "gas":
+                    gas += vehiculo[i].Contenido[y].kiloss
+                if vehiculo[i].Contenido[y].tipo__carga == "normal":
+                    Peso_Normal += vehiculo[i].Contenido[y].kiloss
+                if vehiculo[i].Contenido[y].tipo__carga == "refrigerado":
+                    Peso_Refrigerada += vehiculo[i].Contenido[y].kiloss
+                if vehiculo[i].Contenido[y].tipo__carga == "inflamable":
+                    Peso_Inflamable += vehiculo[i].Contenido[y].kiloss
+                if type(vehiculo[i].Contenido[y]) == (Contenedor_G):
+                    contenedor += 1
+                    peso += vehiculo[i].Contenido[y].kiloss
+                elif type(vehiculo[i].Contenido[y]) == (Contenedor_P):
+                    contenedor += 1
+                    peso += vehiculo[i].Contenido[y].kiloss
+                else:
+                    estanque += 1
+                    peso += vehiculo[i].Contenido[y].kiloss
+        pdf.multi_cell(
+            w=50, h=5, txt=f"Contenedores: {contenedor}\n Estanques: {estanque}", align='C', fill=0, border=1)
+        pdf.multi_cell(
+            w=50, h=5, txt=f"Peso: {peso}", align='C', fill=0, border=1)
+    pdf.multi_cell(
+        w=50, h=5, txt=f"Peso normal: {Peso_Normal}\n Peso Refrigerado: {Peso_Refrigerada}\n Peso Inflamable: {Peso_Inflamable}", align='C', fill=0, border=1)
+
+    pdf.multi_cell(
+        w=50, h=5, txt=f"Peso solido: {solida}\nPeso liquido: {liquida}\nPeso gas: {gas}", align='C', fill=0, border=1)
+    todo = barcos+trenes+aviones+camiones
+    for i in range(len(todo)):
+        pdf.multi_cell(
+            w=199, h=5, txt=f"{todo[i].Contenido}", align='C', fill=0, border=1)
+
+
+try:
+    leer()
+    container()
+    gas_liquido()
+    transporte(b_precio, t_precio, a_precio, c_precio)
+    principal_venetana(b_precio, t_precio, a_precio, c_precio)
+    escribirPDF()
+    pdf.output('hoja.pdf')
+except:
+    print("Ups, ocurrio un error: ", sys.exc_info()[0])
